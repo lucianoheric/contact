76529689eaff8049f461c608ad1f532b8ef0552e
lucianoheric <lucianoheric55@gmail.com>
2020-05-12 10:54:01 -0300

    ajustes heroku

--- src/main/java/com/tribosoftec/contact/domains/Contact.java
+++ src/main/java/com/tribosoftec/contact/domains/Contact.java
@@ -60,23 +60,5 @@ public class Contact implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Contact other = (Contact) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
 	
 }
